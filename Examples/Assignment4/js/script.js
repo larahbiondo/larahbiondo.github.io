14bf913db403c06fc89c845d89dd2600a7a613ed
Larah Biondo <lgb53@miami.edu>
2018-03-28 13:12:21 -0400

    hi

---
+++ Examples/Assignment4/js/script.js
@@ -0,0 +1,101 @@
+$(function(){
+  //Hide stuff
+
+  $('#getStarted, #finish, #previous, .hide, #survey').hide();
+
+
+  //alert("hello");
+  $('#title').on('mouseover', function(){
+    $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+      $('#getStarted').show();
+  });
+
+  $('#title').on('mouseleave', function(){
+    $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+  });
+
+$('#getStarted').on('click', function(){
+  $('.hide').show();
+  $('#getStarted').hide();
+});
+
+$('#finish').on('click', function(){
+  $('.hide, #next, #previous, #finish').hide();
+  $('#survey').show();
+});
+
+
+//Survey
+$('#yes').on('click', function (){
+  $('#surveyTitle').css("background-color", "green");
+  $('#survey').css({"border":"1px solid green",
+  "background":"#ccc"
+  });
+});
+
+$('#no').on('click', function (){
+  $('#surveyTitle').css("background-color", "red");
+  $('#survey').css({"border":"1px solid red",
+  "background":"#ccc"
+  });
+});
+
+
+
+  $('#next').on('click', function(){
+
+    var currentItem = $('li.active');
+    var nextItem = $('li.active').next();
+
+
+    currentItem.toggleClass('active');
+    nextItem.toggleClass('active');
+
+
+  if ($('li').last().hasClass('active')) {
+    $('#next').hide();
+    $('#finish').show();
+  } else {
+    $('#next').show();
+  }
+
+  if ($('li').first().hasClass('active')) {
+  $('#previous').hide();
+  } else {
+  $('#previous').show();
+  }
+
+
+  });
+
+
+  $('#previous').on('click', function(){
+
+    var currentItem = $('li.active');
+    var previousItem = $('li.active').prev();
+
+
+    currentItem.toggleClass('active');
+    previousItem.toggleClass('active');
+
+  if ($('li').last().hasClass('active')) {
+    $('#next').hide();
+    $('#finish').show();
+  } else {
+    $('#next').show();
+  }
+
+  if ($('li').first().hasClass('active')) {
+  $('#previous').hide();
+  } else {
+  $('#previous').show();
+  }
+
+
+  });
+
+
+
+
+
+});
